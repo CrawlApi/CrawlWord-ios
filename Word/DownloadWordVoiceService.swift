bc846154bff84b9ccd4eed5c3db4222e5d93fd1d
marquis <511680713@qq.com>
2016-03-24 14:38:17 +0800

    hidden download voice

--- Word/DownloadWordVoiceService.swift
+++ Word/DownloadWordVoiceService.swift
@@ -19,22 +19,23 @@ class DownloadWordVoiceService {
     
 }
 
+// MARK: - Download Voice
 extension DownloadWordVoiceService{
-    func downloadWordVoice() -> NSURL{
-        Alamofire.download(.GET, voiceURL!) { temporaryURL, response in
-            let fileManager = NSFileManager.defaultManager()
-            let directoryURL = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
-            let pathComponent = response.suggestedFilename
-            let completeURL = directoryURL.URLByAppendingPathComponent(pathComponent!)
-            self.voiceURL = completeURL
 
-            return completeURL
-            }.response { request, response, data, error in
-                if error != nil {
-                    print("Download Error: \(error)")
-                }
-                
-        }
+    func downloadWordVoice() -> NSURL{
+//        Alamofire.download(.GET, voiceURL!) { temporaryURL, response in
+//            let fileManager = NSFileManager.defaultManager()
+//            let directoryURL = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)[0]
+//            let pathComponent = response.suggestedFilename
+//            let completeURL = directoryURL.URLByAppendingPathComponent(pathComponent!)
+//            self.voiceURL = completeURL
+//
+//            return completeURL
+//            }.response { request, response, data, error in
+//                if error != nil {
+//                    print("Download Error: \(error)")
+//                }
+//        }
         return self.voiceURL!
     }
 }
